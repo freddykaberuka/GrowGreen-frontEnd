b905dd8ae0ce586486ae992df08dea5444787fa5
freddy <freddykaberuka2009@gmail.com>
2021-05-08 14:26:19 +0200

    add blog page

--- growgreen/src/App.js
+++ growgreen/src/App.js
@@ -5,7 +5,9 @@ import Footer1 from './components/pages/footer1/footer1';
 import Footer from './components/pages/footer/footer';
 import Contacts from './components/pages/Contacts';
 import Links from './components/pages/landing/links';
-// import ContentInfo from './components/pages/landing/short-info/contentInfo';
+import NavBar from './components/NavBar'
+import landings from './assets/images/ggr-background1.jpg'
+import Blogs from './components/pages/Blogs';
 function App() {
   return (
     <div className="App">
