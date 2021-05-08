32b8400638d062066b968c9ac7ee8b1b16b59ecc
freddy <freddykaberuka2009@gmail.com>
2021-05-08 14:56:04 +0200

    modify blogs

--- growgreen/src/components/pages/Blogs.js
+++ growgreen/src/components/pages/Blogs.js
@@ -8,8 +8,8 @@ function Blogs() {
     return (
         <div>
             
-            <div className="card-deck mt-5 col-sm-12 float-center">
-                <div className="card blog-card">
+            <div className="card-deck mt-5 col-sm-12 blog-card">
+                <div className="card">
                     <img className="card-img-top blogs" src={image1} alt="Card image cap"/>
                     <div className="card-body">
                         <h5 className="card-title">Meeting of Board With Farmers</h5>
@@ -17,7 +17,7 @@ function Blogs() {
                         <p className="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
                     </div>
                 </div>
-                <div className="card blog-card">
+                <div className="card">
                     <img className="card-img-top blogs" src={image2} alt="Card image cap"/>
                     <div className="card-body">
                         <h5 className="card-title">Quality of our seeds</h5>
@@ -25,7 +25,8 @@ function Blogs() {
                         <p className="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
                     </div>
                 </div>
-                <div className="card blog-card">
+                
+                <div className="card">
                     <img className="card-img-top blogs" src={image3} alt="Card image cap"/>
                     <div className="card-body">
                         <h5 className="card-title">Launching Grow Green Rwanda </h5>
@@ -33,7 +34,17 @@ function Blogs() {
                         <p className="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
                     </div>
                 </div>
+                <div className="card">
+                    <img className="card-img-top blogs" src={image3} alt="Card image cap"/>
+                    <div className="card-body">
+                        <h5 className="card-title">Launching Grow Green Rwanda </h5>
+                        <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <p className="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                    </div>
+                </div>
+                
             </div>
+            
         </div>
     )
 }
