5353c2bb166aca033da03f104a7ffa609b3c806d
freddy <freddykaberuka2009@gmail.com>
2021-05-22 16:08:49 +0200

    nodifie footer

--- growgreen/src/components/pages/footer/footer.js
+++ growgreen/src/components/pages/footer/footer.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './footer.css'
+
 function Footer() {
     return (
         <div>
@@ -11,48 +12,47 @@ function Footer() {
                             <div className="col-md-3 col-sm-6">
                             {/* <!--Column1 */}
                                 <div className="footer-pad">
-                                    <h4>Heading 1</h4>
+                                    <h4>What We Do</h4>
                                     <ul class="list-unstyled">
                                         <li><a href="#"></a></li>
-                                        <li><a href="#">Payment Center</a></li>
-                                        <li><a href="#">Contact Directory</a></li>
-                                        <li><a href="#">Forms</a></li>
-                                        <li><a href="#">News and Updates</a></li>
-                                        <li><a href="#">FAQs</a></li>
+                                        <li><a href="#">Farmers First</a></li>
+                                        <li><a href="#">Our Model</a></li>
+                                        <li><a href="#">How we Grow</a></li>
+                                        <li><a href="#">Insights and Data Library</a></li>
+    
                                     </ul>
                                 </div>
                             </div>
                             <div className="col-md-3 col-sm-6">
                             {/* <!--Column1--> */}
                                 <div className="footer-pad">
-                                    <h4>Heading 2</h4>
+                                    <h4>Who We Are</h4>
                                     <ul className="list-unstyled">
-                                        <li><a href="#">Website Tutorial</a></li>
-                                        <li><a href="#">Accessibility</a></li>
-                                        <li><a href="#">Disclaimer</a></li>
-                                        <li><a href="#">Privacy Policy</a></li>
-                                        <li><a href="#">FAQs</a></li>
-                                        <li><a href="#">Webmaster</a></li>
+                                        <li><a href="#">Our Impact</a></li>
+                                        <li><a href="#">Press and Awards</a></li>
+                                        <li><a href="#">Board and Advisors</a></li>
+                                        <li><a href="#">Reports</a></li>
                                     </ul>
                                 </div>
                             </div>
                             <div className="col-md-3 col-sm-6">
                             {/* <!--Column1--> */}
                                 <div className="footer-pad">
-                                    <h4>Heading 3</h4>
+                                    <h4>Careers</h4>
                                     <ul className="list-unstyled">
-                                        <li><a href="#">Parks and Recreation</a></li>
-                                        <li><a href="#">Public Works</a></li>
-                                        <li><a href="#">Police Department</a></li>
-                                        <li><a href="#">Fire</a></li>
-                                        <li><a href="#">Mayor and City Council</a></li>
-                                        <li><a href="#"></a></li>
+                                        <li><a href="#">Why Work Here</a></li>
+                                        <li><a href="#">Careers FAQ</a></li>
+                                        <li><a href="#">Careers Blog</a></li>
+                                        <li><a href="#">Job Openings</a></li>
+                                        
                                     </ul>
                                 </div>
                             </div>
     	                    <div className="col-md-3">
     		                    <h4>Follow Us</h4>
                                     <ul className="social-network social-circle">
+                                        <li><a href="#" class="icoFacebook" title="Facebook"><i className="fa fa-facebook"></i></a></li>
+                                        <li><a href="#" class="icoLinkedin" title="Linkedin"><i className="fa fa-linkedin"></i></a></li>
                                         <li><a href="#" class="icoFacebook" title="Facebook"><i className="fa fa-facebook"></i></a></li>
                                         <li><a href="#" class="icoLinkedin" title="Linkedin"><i className="fa fa-linkedin"></i></a></li>
                                     </ul>				
