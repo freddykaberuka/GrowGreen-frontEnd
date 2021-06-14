d9a87e68e011982fc53a620bf9d8efe26f7f0ddd
freddy <freddykaberuka2009@gmail.com>
2021-06-14 19:05:19 +0200

    correct color

--- growgreen/src/components/pages/about/purpose.js
+++ growgreen/src/components/pages/about/purpose.js
@@ -7,18 +7,18 @@ function Purpose() {
     return (
         <div>
             <div>
-            <div className="card-deck mt-5 col-sm-12 row-cols-1">
+            <div className="card-deck mt-5 col-sm-12 row-cols-1 pb-6">
                 <div className="card">
                     <img className="card-img-top row-cols-1 fit-image" src={image1} alt="Card image cap"/>
-                    <div className='font-weight-normal pt-4 title'>Purpose and Value</div>
+                    <div className='font-weight-normal pt-4 title pb-3'>Purpose and Value</div>
                 </div>
                 <div class="card">
                     <img className="card-img-top fit-image" src={image2} alt="Card image cap"/>
-                    <div className='font-weight-normal pt-4 title'>Our Mission</div>
+                    <div className='font-weight-normal pt-4 title pb-3'>Our Mission</div>
                 </div>
                 <div class="card">
                     <img className="card-img-top fit-image" src={image2} alt="Card image cap"/>
-                    <div className='font-weight-normal pt-4 title'>Leadership</div>
+                    <div className='font-weight-normal pt-4 title pb-3'>Leadership</div>
                 </div>
                 
             </div>
