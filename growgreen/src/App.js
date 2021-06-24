824688f99b300575cd2bf8b95c12fc144dbf4589
freddy <freddykaberuka2009@gmail.com>
2021-06-24 14:43:55 +0200

    routing page

--- growgreen/src/App.js
+++ growgreen/src/App.js
@@ -1,23 +1,24 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
-import Subscribe from './components/pages/footer1/subscribe/subscribe';
-import Footer1 from './components/pages/footer1/footer1';
-import Footer from './components/pages/footer/footer';
-import Contacts from './components/pages/Contacts';
-import Links from './components/pages/landing/links';
-import NavBar from './components/NavBar'
-import landings from './assets/images/ggr-background1.jpg'
-import Blogs from './components/pages/Blogs';
+import Home from './components/pages/landing/home';
+import Blog from './components/pages/blog/blog'
+import {Route, Link, BrowserRouter as Router} from 'react-router-dom'
+import Topheader from './components/topheader';
+import About from './components/pages/about/about';
+import { Switch } from 'react-router-dom/cjs/react-router-dom.min';
+import Contact from './components/pages/contact/contact';
 function App() {
   return (
     <div className="App">
-      <Links/>
-      <div className="pt-10 pl-14 pb-10">
-      <Contacts/>
-      </div>
-     <Subscribe/>
-     
-     <Footer/>
+      
+      <Router>
+        <Switch>
+          <Route path='/' exact component={Home} exact={ true }/>
+          <Route path='/article' component={Blog}/>
+          <Route path='/about' component ={About}/>
+          <Route path='/contact' component ={Contact}/>
+        </Switch>
+      </Router>
     </div>
   );
 }
