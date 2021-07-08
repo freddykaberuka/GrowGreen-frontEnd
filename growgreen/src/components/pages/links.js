8fc1c07fe96f1b2220bee135a934def2d5133720
freddy <freddykaberuka2009@gmail.com>
2021-07-08 12:54:01 +0200

    modifie navlink and next button

--- growgreen/src/components/pages/links.js
+++ growgreen/src/components/pages/links.js
@@ -11,16 +11,12 @@ function Links() {
   				<div class="collapse navbar-collapse mx0" id="collapsibleNavbar">
   					<ul className="navbar-nav nav">
 						<li className="nav-item active pl-3 pr-3 ml-0"><NavLink to="/" class="nav-link" exact={true}>Home</NavLink></li>
-						<li className="nav-item px-4 pl-3"><NavLink to="/about" class="nav-link nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">About Us</NavLink>
-						<div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-          					<Link to="#" className="dropdown-item">Action</Link>
-          					<Link to="#" className="dropdown-item">Another action</Link>
-          					<Link to="#" className="dropdown-item">Something else here</Link>
-        				</div>
+						<li className="nav-item px-4 pl-3"><NavLink to="/about" class="nav-link">About Us</NavLink>
+						
 						</li>
 						<li className="nav-item px-4 pl-3"><NavLink to="/article" class="nav-link">Blogs</NavLink></li>
 						<li className="nav-item px-4 pl-3"><NavLink to="/photo" class="nav-link">Gallery</NavLink></li>
-						<li className="nav-item px- pl-3"><NavLink to="/contact" class="nav-link">Contact Us</NavLink></li>
+						<li className="nav-item px-4 pl-3"><NavLink to="/contact" class="nav-link">Contact Us</NavLink></li>
 						<li className="nav-item px-2 pl-3"><NavLink to="/contact" class="nav-link">Donate</NavLink></li>
 					</ul>
   				</div>  
