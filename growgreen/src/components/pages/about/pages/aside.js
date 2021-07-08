50111f1efe3d9f9c0c3dd3622fe1439b2c23b931
freddy <freddykaberuka2009@gmail.com>
2021-07-08 12:50:49 +0200

    add link aside

--- growgreen/src/components/pages/about/pages/aside.js
+++ growgreen/src/components/pages/about/pages/aside.js
@@ -8,11 +8,11 @@ function Aside() {
         <div className='mt-20 mb-4 aside'>
             <h5 className='line-cads'>INSIDE ABOUT US</h5>
             <ul className="listing-aside line-cads" style={{textDecoration: 'none', color:'black'}}>
-                <li><Link to='#'>Purpose and Values</Link></li>
-                <li><Link to=''>Our Vision</Link></li>
-                <li><Link to=''>Leadership</Link></li>
-                <li><Link to=''>Our Offices</Link></li>
-                <li><Link to=''>Contact Us</Link></li>
+                <li><Link to='/purpose'>Purpose and Values</Link></li>
+                <li><Link to='/mission'>Our Vision</Link></li>
+                <li><Link to='/leadership'>Leadership</Link></li>
+                <li><Link to='/office'>Our Offices</Link></li>
+                <li><Link to='/contact'>Contact Us</Link></li>
             </ul>
         </div>
     )
