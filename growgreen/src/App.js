1c6732fa4e4901158170e9da13eca57ee39b50af
freddy <freddykaberuka2009@gmail.com>
2021-07-09 11:17:41 +0200

    add workwith us page

--- growgreen/src/App.js
+++ growgreen/src/App.js
@@ -10,7 +10,7 @@ import Contact from './components/pages/contact/contact';
 import Gallery from './components/pages/gallery/gallery';
 // import SingleWedo from './components/pages/landing/secondsection/singleWedo';
 // import SingleImpact from './components/pages/landing/secondsection/singleImpact';
-import SingleWork from './components/pages/landing/secondsection/singleWork';
+// import SingleWork from './components/pages/landing/secondsection/singleWork';
 import Blogging from './components/pages/blog/blog1';
 import SinglePurpose from './components/pages/about/pages/singlePurpose';
 import SingleMission from './components/pages/about/pages/singleMission';
@@ -18,6 +18,7 @@ import SingleLeadership from './components/pages/about/pages/singleLeadership';
 import SingleOffice from './components/pages/about/pages/singleOffice';
 import SingleWedo from './components/pages/about/pages/singleWhatwedo';
 import SingleImpact from './components/pages/about/pages/singleImpact';
+import SingleWork from './components/pages/about/pages/singleWork';
 function App() {
   return (
     <div className="App">
@@ -30,13 +31,14 @@ function App() {
           <Route path='/photo' component ={Gallery} />
           <Route path='/wedo' component={SingleWedo}/>
           {/* <Route path='/impact' component={SingleImpact}/> */}
-          <Route path='/work' component={SingleWork}/>
+          {/* <Route path='/work' component={SingleWork}/> */}
           <Route path='/singleblog' component={Blogging}/>
           <Route path='/purpose' component={SinglePurpose}/>
           <Route path='/mission' component={SingleMission}/>
           <Route path='/leadership' component={SingleLeadership}/>
           <Route path='/whatwedo' component={SingleWedo}/>
           <Route path='/impact' component={SingleImpact}/>
+          <Route path='/workwithus' component={SingleWork}/>
         </Switch>
       </Router>
     </div>
